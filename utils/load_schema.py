f390272f1752b2b0773b637538a11d02540e6e24
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-12-05 23:50:13 +0300

    fix path in utils.load_schema

--- utils/load_schema.py
+++ utils/load_schema.py
@@ -1,7 +1,10 @@
 import json
+import os
 
 
 def load_schema(path):
-    with open(path) as f:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, path)
+    with open(file_path) as f:
         schema = json.load(f)
         return schema
